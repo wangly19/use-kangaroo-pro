f73166c98ada7dd1d3311d0c41a84b34b29cfc5f
wangly19 <1418202350@qq.com>
2021-03-27 20:25:19 +0800

    config(env): 添加环境变量打包区分

---
+++ config/config.pro.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  externals: {
+    react: 'window.React',
+  },
+  scripts: ['https://unpkg.com/react@17.0.1/umd/react.production.min.js'],
+});
--- typings.d.ts
+++ typings.d.ts
@@ -2,7 +2,17 @@ declare module '*.css';
 declare module '*.less';
 declare module '*.png';
 declare module '*.svg' {
-  export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement
-  const url: string
-  export default url
+  export function ReactComponent(
+    props: React.SVGProps<SVGSVGElement>,
+  ): React.ReactElement;
+  const url: string;
+  export default url;
 }
+
+/** [type]: 定义define中可能用到的类型变量 */
+
+declare const INTERFACE_URL: string; // 接口请求地址
+
+declare const OSS_URL: string; // OSS资源地址
+
+declare const APP_SELECT_KEY: string; // 系统调用密钥
